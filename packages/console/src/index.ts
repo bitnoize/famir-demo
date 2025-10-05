c459355c6867e2ac90e6d7928753e47cb66f5453
Dmitry Krutikov <d.krutikov@gmail.com>
2025-10-05 03:21:31 +0300

    Sync

--- packages/console/src/index.ts
+++ packages/console/src/index.ts
@@ -1,10 +1,24 @@
 import { DIContainer } from '@famir/common'
-import { composeCampaign, composeProxy, bootstrap } from '@famir/console'
+import {
+  composeCampaign,
+  composeProxy,
+  composeTarget,
+  composeLure,
+  bootstrap,
+  composeRedirector,
+  composeSession,
+  composeMessage
+} from '@famir/console'
 
 try {
   await bootstrap((container: DIContainer) => {
     composeCampaign(container)
     composeProxy(container)
+    composeTarget(container)
+    composeRedirector(container)
+    composeLure(container)
+    composeSession(container)
+    composeMessage(container)
   })
 } catch (error) {
   console.error(`Bootstrap failed`, { error })
